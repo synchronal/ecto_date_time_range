530480e83a08f0725ed4d72d841b9b62d90d24ca
Eric Saxby <sax@livinginthepast.org>
2022-07-12 17:57:36 -0700

    Release v0.4.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,7 @@
 # Change Log
 
+## 0.4.0
+
 - Add `Ecto.DateTimeRange.Time` type, representing a naive time range.
 
 ## 0.3.1
--- mix.exs
+++ mix.exs
@@ -24,7 +24,7 @@ defmodule EctoDateTimeRange.MixProject do
       preferred_cli_env: [credo: :test, dialyzer: :test],
       source_url: @scm_url,
       start_permanent: Mix.env() == :prod,
-      version: "0.3.1"
+      version: "0.4.0"
     ]
   end
 
