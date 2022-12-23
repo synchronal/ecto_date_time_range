0fefcf94488a8990ab1109d1f63319421b789c3d
Eric Saxby <sax@livinginthepast.org>
2022-12-22 16:30:10 -0800

    Add db scripts

--- .medic/doctor.exs
+++ .medic/doctor.exs
@@ -4,8 +4,8 @@
   {Medic.Checks.Hex, :local_hex_installed?},
   {Medic.Checks.Hex, :local_rebar_installed?},
   {Medic.Checks.Hex, :packages_installed?},
-  {Medic.Checks.Postgres, :running?}
-  # {Medic.Checks.Postgres, :correct_version_running?},
-  # {Medic.Checks.Postgres, :role_exists?},
-  # {Local.Checks.Postgres, :correct_data_directory?},
+  {Medic.Checks.Postgres, :running?, [remedy: "bin/dev/db-start", username: System.fetch_env!("USER")]},
+  {Medic.Checks.Postgres, :role_exists?},
+  {Medic.Checks.Postgres, :correct_version_running?, [remedy: "bin/dev/db-restart"]},
+  {Medic.Checks.Postgres, :correct_data_directory?, [remedy: "bin/dev/db-restart"]}
 ]
